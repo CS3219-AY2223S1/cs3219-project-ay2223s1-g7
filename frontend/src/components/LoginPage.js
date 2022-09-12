b977c4bc2a14f8519580da6a50c6625c6c1bb8cb
KennethEer <59533849+KennethEer@users.noreply.github.com>
2022-09-12 14:42:47 +0800

    implement login

--- frontend/src/components/LoginPage.js
+++ frontend/src/components/LoginPage.js
@@ -16,12 +16,83 @@ import {STATUS_CODE_CONFLICT, STATUS_CODE_CREATED} from "../constants";
 import {Link} from "react-router-dom";
 
 function LoginPage() {
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+    const [isDialogOpen, setIsDialogOpen] = useState(false)
+    const [dialogTitle, setDialogTitle] = useState("")
+    const [dialogMsg, setDialogMsg] = useState("")
+    const [isLoginSuccess, setIsLoginSuccess] = useState(false)
+
+    const handleLogin = async () => {
+        setIsLoginSuccess(false)
+        const res = await axios.post(URL_USER_SVC + '/login', { username, password })
+            .catch((err) => {
+                if (err.response.status === STATUS_CODE_CONFLICT) {
+                    setErrorDialog('Invalid credentials')
+                } else {
+                    setErrorDialog('Please try again later')
+                }
+            })
+        if (res && res.status === STATUS_CODE_CREATED) {
+            setSuccessDialog('Successfully logged in')
+            setIsLoginSuccess(true)
+        }
+    }
+
+    const closeDialog = () => setIsDialogOpen(false)
+
+    const setSuccessDialog = (msg) => {
+        setIsDialogOpen(true)
+        setDialogTitle('Success')
+        setDialogMsg(msg)
+    }
+
+    const setErrorDialog = (msg) => {
+        setIsDialogOpen(true)
+        setDialogTitle('Error')
+        setDialogMsg(msg)
+    }
+
     return (
-        
-        <div className="App">
-            Hello
-        </div>
-    );
+        <Box display={"flex"} flexDirection={"column"} width={"30%"}>
+            <Typography variant={"h3"} marginBottom={"2rem"}>Login</Typography>
+            <TextField
+                label="Username"
+                variant="standard"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                sx={{marginBottom: "1rem"}}
+                autoFocus
+            />
+            <TextField
+                label="Password"
+                variant="standard"
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                sx={{marginBottom: "2rem"}}
+            />
+            <Box display={"flex"} flexDirection={"row"} justifyContent={"flex-end"}>
+                <Button variant={"outlined"} onClick={handleLogin}>Login</Button>
+            </Box>
+
+            <Dialog
+                open={isDialogOpen}
+                onClose={closeDialog}
+            >
+                <DialogTitle>{dialogTitle}</DialogTitle>
+                <DialogContent>
+                    <DialogContentText>{dialogMsg}</DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    {isLoginSuccess
+                        ? <Button component={Link} to="/home">Proceed</Button>
+                        : <Button onClick={closeDialog}>Done</Button>
+                    }
+                </DialogActions>
+            </Dialog>
+        </Box>
+    )
 }
 
 export default LoginPage;
