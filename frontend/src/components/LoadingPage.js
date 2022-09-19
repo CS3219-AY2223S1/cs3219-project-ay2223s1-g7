b049010886da55af77866cc5fbeb577efe143543
xingrong123 <teyxingrong@gmail.com>
2022-09-19 15:15:13 +0800

    Add question page

--- frontend/src/components/LoadingPage.js
+++ frontend/src/components/LoadingPage.js
@@ -4,45 +4,19 @@ import {
     TextField,
     Typography
 } from "@mui/material";
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
-import {URL_USER_SVC, URL_MATCH_SVC} from "../configs";
-import {STATUS_CODE_CONFLICT, STATUS_CODE_CREATED} from "../constants";
-import {Link, useNavigate} from "react-router-dom";
-import { io } from "socket.io-client"
+import { URL_USER_SVC, URL_MATCH_SVC } from "../configs";
+import { STATUS_CODE_CONFLICT, STATUS_CODE_CREATED } from "../constants";
+import { Link, useNavigate } from "react-router-dom";
 
 
-// const socket = io(URL_MATCH_SVC)
+function LoadingPage(props) {
 
-function LoadingPage() {
-    const navigate = useNavigate()
-    const [socket, setSocket] = useState(io())
-    const [text, setText] = useState("")
-
-    useEffect(() => {
-        socket.on('connection')
-        socket.on("received_message", (data) => {
-            setText(data)
-        })
-    }, [socket])
-
-    const handleExit = async () => {
-        /* setSocket(io(URL_MATCH_SVC, {
-            query: {
-                "username": username,
-                "difficulty": difficulty
-            }
-        }))
-        */
-        navigate("/home")
-    }
-
-
-    
     return (
         <Box display={"flex"} flexDirection={"column"} alignSelf={"center"} width={"50%"} sx={{ 'button': { m: 1 } }}>
             <Typography variant={"h3"} textAlign={"center"} marginBottom={"2rem"}>Loading</Typography>
-            <Button variant={"contained"} color={"error"} onClick={() => handleExit()}>Exit</Button>
+            <Button variant={"contained"} color={"error"} onClick={() => props.handleExit()}>Exit</Button>
         </Box>
     )
 }
--- frontend/src/components/QuestionSelectionPage.js
+++ frontend/src/components/QuestionSelectionPage.js
@@ -4,88 +4,15 @@ import {
     TextField,
     Typography
 } from "@mui/material";
-import { useEffect, useState } from "react";
-import axios from "axios";
-import { URL_USER_SVC, URL_MATCH_SVC } from "../configs";
-import { STATUS_CODE_CONFLICT, STATUS_CODE_CREATED } from "../constants";
-import { Link, useNavigate } from "react-router-dom";
-import { io } from "socket.io-client"
-import { getCookie, setCookie } from "../utils/cookies"
 
-// const socket = io(URL_MATCH_SVC)
-
-function QuestionSelectionPage() {
-    const navigate = useNavigate()
-    const [difficulty, setDifficulty] = useState("")
-    const [socket, setSocket] = useState(io())
-    const [text, setText] = useState("")
-
-    // useEffect(() => {
-    //     window.addEventListener("beforeunload", alertUser);
-    //     return () => {
-    //         window.removeEventListener("beforeunload", alertUser);
-    //     };
-    // }, []);
-    // const alertUser = (e) => {
-    //     e.preventDefault();
-    //     e.returnValue = "";
-    // };
-    window.onbeforeunload = (event) => {
-        const e = event || window.event;
-        // Cancel the event
-        e.preventDefault();
-        if (e) {
-            e.returnValue = ''; // Legacy method for cross browser support
-        }
-        return ''; // Legacy method for cross browser support
-    };
-    useEffect(() => {
-        socket.on('connection')
-
-        socket.on('matchFail', () => {
-            // handle match fail
-            console.log("match failed")
-        })
-
-        socket.on('matchSuccess', (roomName) => {
-            // handle match success
-            console.log("match Success")
-            setCookie("room_name", roomName, 5)
-        })
-
-        socket.on("received_message", (data) => {
-            setText(data)
-        })
-    }, [socket])
-
-    const handleMatching = async (difficulty) => {
-        console.log(difficulty)
-        setDifficulty(difficulty)
-        let username = getCookie("user")
-        setSocket(io(URL_MATCH_SVC, {
-            query: {
-                "username": username,
-                "difficulty": difficulty
-            },
-            closeOnBeforeunload: false
-        }))
-
-        navigate("/loading")
-    }
-
-    const handleTextChange = (event) => {
-        var value = event.target.value
-        socket.emit('send_message', value)
-        setText(value)
-    }
+function QuestionSelectionPage(props) {
 
     return (
         <Box display={"flex"} flexDirection={"column"} alignSelf={"center"} width={"50%"} sx={{ 'button': { m: 1 } }}>
             <Typography variant={"h3"} textAlign={"center"} marginBottom={"2rem"}>Choose difficulty</Typography>
-            <Button variant={"contained"} color={"success"} onClick={() => handleMatching("EASY")}>EASY</Button>
-            <Button variant={"contained"} color={"warning"} onClick={() => handleMatching("MEDIUM")}>MEDIUM</Button>
-            <Button variant={"contained"} color={"error"} onClick={() => handleMatching("HARD")}>HARD</Button>
-            <TextField multiline rows={7} onChange={handleTextChange} value={text} />
+            <Button variant={"contained"} color={"success"} onClick={() => props.handleMatching("EASY")}>EASY</Button>
+            <Button variant={"contained"} color={"warning"} onClick={() => props.handleMatching("MEDIUM")}>MEDIUM</Button>
+            <Button variant={"contained"} color={"error"} onClick={() => props.handleMatching("HARD")}>HARD</Button>
         </Box>
     )
 }
