711fa28213577b4c1e943f332bcdc3d47283b3d3
xingrong123 <teyxingrong@gmail.com>
2022-11-03 20:05:21 +0800

    add question service deployment

--- question-service/controller/question-controller.js
+++ question-service/controller/question-controller.js
@@ -1,30 +1,35 @@
-import { ormCreateQuestion as _createQuestion, ormSearchAllQuestionsByDifficulty as _searchAllQuestionsByDifficulty, 
-    ormSearchQuestionByDifficulty as _searchQuestionByDifficulty, ormDeleteQuestion as _deleteQuestion, ormSearchQuestionByTitle as _searchQuestionByTitle, ormSearchAllQuestionsAttempted as _searchAllQuestionsAttempted, ormAttemptQuestion as _attemptQuestion} from '../model/question-orm.js'
+import {
+    ormCreateQuestion as _createQuestion, ormSearchAllQuestionsByDifficulty as _searchAllQuestionsByDifficulty,
+    ormSearchQuestionByDifficulty as _searchQuestionByDifficulty, ormDeleteQuestion as _deleteQuestion, ormSearchQuestionByTitle as _searchQuestionByTitle, 
+    ormSearchAllQuestionsAttempted as _searchAllQuestionsAttempted, ormAttemptQuestion as _attemptQuestion
+} from '../model/question-orm.js'
 import redis from "redis"
-const redisClient = redis.createClient()
+
+const REDIS_HOST = process.env.ENV == "PROD" ? { url: process.env.REDIS_HOST } : '';
+const redisClient = redis.createClient(REDIS_HOST)
 if (!redisClient) {
     console.log("cannot connect to redis");
-  } else {
+} else {
     console.log("connected to redis");
-  }
-  
+}
+
 export async function addQuestion(req, res) {
     try {
         const { title, question, difficulty } = req.body;
         if (title && question && difficulty) {
             const resp = await _createQuestion(title, question, difficulty)
-           
+
             if (resp.err) {
-                return res.status(409).json({message: 'Could not create a new question!'});
+                return res.status(409).json({ message: 'Could not create a new question!' });
             } else {
                 console.log(`Created new question ${title} successfully!`)
-                return res.status(201).json({message: `Created new question ${title} successfully!`});
-            } 
+                return res.status(201).json({ message: `Created new question ${title} successfully!` });
+            }
         } else {
-            return res.status(400).json({message: 'Title and/or Question and/or Difficulty are missing!'});
+            return res.status(400).json({ message: 'Title and/or Question and/or Difficulty are missing!' });
         }
     } catch (err) {
-        return res.status(500).json({message: 'Database failure when creating new question!'})
+        return res.status(500).json({ message: 'Database failure when creating new question!' })
     }
 }
 
@@ -34,101 +39,99 @@ export async function deleteQuestion(req, res) {
         const { title } = req.body;
         const deleteResp = await _deleteQuestion(title);
         if (deleteResp.err) {
-            return res.status(409).json({message: 'Unable to delete question'});
+            return res.status(409).json({ message: 'Unable to delete question' });
         }
 
-        return res.status(200).json({message: `Delete successful`});
-            
+        return res.status(200).json({ message: `Delete successful` });
+
     } catch (err) {
-       return res.status(500).json({message: 'Database failure when deleting question!'})
+        return res.status(500).json({ message: 'Database failure when deleting question!' })
     }
 }
 
 
 export async function getQuestion(req, res) {
     try {
-        
-        const {difficulty, userOne , userTwo}= req.body
+
+        const { difficulty, userOne, userTwo } = req.body
         console.log("DIFFICULTY IS: " + difficulty)
         const resp = await _searchQuestionByDifficulty(difficulty, userOne, userTwo);
         if (resp.err) {
-            return res.status(409).json({message: 'Could not find question!'});
+            return res.status(409).json({ message: 'Could not find question!' });
         } else {
-            return res.status(201).json({question: resp});
-        } 
-        
+            return res.status(201).json({ question: resp });
+        }
+
     } catch (err) {
-        return res.status(500).json({message: 'Database failure when finding question!'})
+        return res.status(500).json({ message: 'Database failure when finding question!' })
     }
 }
 
 
 export async function getAllQuestions(req, res) {
     try {
-        
-        const {difficulty}= req.body
+
+        const { difficulty } = req.body
         const resp = await _searchAllQuestionsByDifficulty(difficulty);
         console.log(resp)
         if (resp.err) {
-            return res.status(409).json({message: 'Could not find question!'});
+            return res.status(409).json({ message: 'Could not find question!' });
         } else {
-            return res.status(201).json({question: resp});
-        } 
-        
+            return res.status(201).json({ question: resp });
+        }
+
     } catch (err) {
-        return res.status(500).json({message: 'Database failure when finding question!'})
+        return res.status(500).json({ message: 'Database failure when finding question!' })
     }
 }
 
 
 export async function getAllQuestionsAttempted(req, res) {
     try {
-        const {user}= req.body
+        const { user } = req.body
 
         redisClient.get(`questions`, async (error, questions) => {
             if (error) console.error(error);
             if (questions != null) {
                 console.log("Hit")
-                return res.status(201).json({question: JSON.parse(questions)});
+                return res.status(201).json({ question: JSON.parse(questions) });
             } else {
                 console.log("Miss")
                 const resp = await _searchAllQuestionsAttempted(user);
                 console.log(resp)
                 if (resp.err) {
-                    return res.status(409).json({message: 'Could not find question!'});
+                    return res.status(409).json({ message: 'Could not find question!' });
                 } else {
                     redisClient.setex(
                         `questions`,
                         1000,
                         JSON.stringify(resp)
                     )
-                    return res.status(201).json({question: resp});
-                } 
+                    return res.status(201).json({ question: resp });
+                }
             }
-        }
-        )
-        
-        
+        })
+
     } catch (err) {
-        return res.status(500).json({message: 'Database failure when finding question!'})
+        return res.status(500).json({ message: 'Database failure when finding question!' })
     }
 
 }
 
 export async function attemptQuestion(req, res) {
     try {
-        
-        const {title, user}= req.body
+
+        const { title, user } = req.body
         const resp = await _attemptQuestion(title, user);
         console.log(resp)
         if (resp.err) {
-            return res.status(409).json({message: 'Could not find question!'});
+            return res.status(409).json({ message: 'Could not find question!' });
         } else {
-            return res.status(201).json({question: resp});
-        } 
-        
+            return res.status(201).json({ question: resp });
+        }
+
     } catch (err) {
-        return res.status(500).json({message: 'Database failure when finding question!'})
+        return res.status(500).json({ message: 'Database failure when finding question!' })
     }
 
 }
